7d7875926d4bd580e460b035d6971f0e1f597e62
Nouredine KRIKOU <97070762+krikounoureddine@users.noreply.github.com>
2024-12-02 00:19:46 +0100

    Add files via upload

---
+++ modules/authParams.js
@@ -0,0 +1,8 @@
+export const pa = {
+    'navbar':d3.select('#navbarMain'),
+    mail:'krikounoureddine@gmail.com',
+    apiOmk:'http://localhost/omeka-s/api/',
+    ident: 'Wu2JU2niVUCKCVPlMtYNTkNYUZW6ehHg',
+    key:'1Y98gQN1FHYU84TNdGrGIZlx3iZz2JRt',
+    keyGitHub:''
+}
